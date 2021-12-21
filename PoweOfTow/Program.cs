f97ea688d296d73cf98852d5e85bfe00c043aa1c
Jaegyeom <jaegyeom1215@gmail.com>
2021-12-21 14:41:14 -0800

    PowerOfTwo

---
+++ PoweOfTow/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PoweOfTow
+{
+
+    public class Solution
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n == 1)
+            {
+                return true;
+            }
+
+            int num = 1;
+            for (int i = 0; i < n; i++) // 4
+            {
+                num *= 2;
+                if (num == n)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.IsPowerOfTwo(1)); //t
+            Console.WriteLine(solution.IsPowerOfTwo(2));
+            Console.WriteLine(solution.IsPowerOfTwo(6));
+            Console.WriteLine(solution.IsPowerOfTwo(8));
+
+
+
+        }
+    }
+}
